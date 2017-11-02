29c21195e536bf22548a03dbf0aad4cc0869ad61
piyushapatel94 <piyusha208@gamil.com>
2017-11-02 17:37:29 +0530

    google map path

--- app/controllers/mymap.js
+++ app/controllers/mymap.js
@@ -177,39 +177,11 @@ export default Ember.Controller.extend({
                             this.set('Dlan', 80.2706974);
                             
                         }
-                        var Slat =this.get('Slat');
-                        var Slan =this.get('Slan');
-                        var Dlat =this.get('Dlat');
-                        var Dlan =this.get('Dlan');
-                        console.log("Slat",Slat);
-                       var directionsDisplay = new google.maps.DirectionsRenderer();
-                       var mapOptions = {
-                        zoom: 7,
                         
-                      };
-                      map = new google.maps.Map(this.get('map'), mapOptions);
-                      directionsDisplay.setMap(map);
-                        var directionsService = new google.maps.DirectionsService();
-                       var start = new google.maps.LatLng(18.5206624, 73.8567415);
-                       // var end = new google.maps.LatLng(Dlat, Dlan);
-                        var end = new google.maps.LatLng(13.0826809, 80.2706974);
-                       var request = {
-                        origin: start,
-                        destination: end,
-                        travelMode: google.maps.TravelMode.DRIVING
-                      };
-                      directionsService.route(request, function(response, status) {
-                        if (status == google.maps.DirectionsStatus.OK) {
-                          directionsDisplay.setDirections(response);
-                          directionsDisplay.setMap(map);
-                        } else {
-                          alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
-                        }
-                      });
+                      
                     
                   
-                  // google.maps.event.addDomListener(window, 'load', initialize);
-        ///mapLocation();
+               
         
                   
         }
